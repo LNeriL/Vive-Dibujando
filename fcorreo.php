3474973dbb486961a70b039b24196a9a7db3d38b
LNeriL <78660636+LNeriL@users.noreply.github.com>
2021-02-09 18:07:58 -0600

    Add files via upload

---
+++ fcorreo.php
@@ -0,0 +1,15 @@
+<?php
+    $destinatario = 'LJZNERI23@gmai.com';
+
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $telefono = $_POST['telefono'];
+    $asunto = $_POST['asunto'];
+
+    $header = "Correo de Web Vive Dibujando";
+    $mensajeCompleto = $mensaje . "\nAtt:" . $nombre;
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo "<script>alert('Correo Enviado Exitosamente')</script>";
+    echo "<script> setTimeout(\"location.href='formulario.html'\",1000)</script>";
+?>
